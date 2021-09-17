0ec9f06f8419b86f113469175580d0735008730f
harish kumar <87074160+harishKumar1307@users.noreply.github.com>
2021-09-17 11:07:22 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,10 @@
 terraform {
 }
   provider "azurerm" {
-      source  = "hashicorp/azurerm"
+//      source  = "hashicorp/azurerm"
       version = "=2.46.0"
       features {}
     }
-  }
 
 resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
