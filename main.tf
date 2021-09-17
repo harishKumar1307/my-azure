b75eed3b338f886dfe78e3db2a816898766fb629
harish kumar <87074160+harishKumar1307@users.noreply.github.com>
2021-09-17 11:04:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,14 +11,13 @@
 
 
 terraform {
-  required_providers {
-    azurerm = {
+}
+  provider "azurerm" {
       source  = "hashicorp/azurerm"
       version = ">=2.46.0"
       features {}
     }
   }
-}
 
 resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
